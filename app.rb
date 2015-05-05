36a873a75855677893b6550702d56cee899acd5c
karthikreddy87 <karthikreddy87@users.noreply.github.com>
2015-05-05 03:32:48 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "This is Karthik doing the Openshift lab3!\n"+
+  "This is Karthik doing the Openshift!\n"+
 # ENV values are generated during template processing
 # and then passed to the container when openshift launches it.
   "User is #{ENV['ADMIN_USERNAME']}\n"+
